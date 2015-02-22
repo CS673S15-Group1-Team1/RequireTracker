00d8ae61a658bce3f60df8d0a9f6c0f9c73c3d93
Max Almanzor <max.almanzor@gmail.com>
2015-02-22 18:11:24 -0500

    Story 88868134 add log out button

--- ProjMgmt/projects/views.py
+++ ProjMgmt/projects/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 import models
 from django.shortcuts import render
+import django.contrib.auth
 
 @login_required(login_url='/accounts/login/')
 def listProjects(request):
 	context = {'projects' : models.getProjectsForUser(request.user.id)}
 	return render(request, 'projects.html', context)
+	
+@login_required	
+def logout(request):
+	django.contrib.auth.logout(request)
+	return HttpResponse("Log Out Successful")
+	
