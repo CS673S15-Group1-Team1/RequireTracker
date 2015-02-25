320e4135d778ef942972a9fd9b66e1236c3708da
jaredmael <jaredmael@gmail.com>
2015-02-25 00:32:20 -0500

    CSS static folder added, Login updated, Homepage
    CSS static folder was added.  Homepage added, Login page update with
    CSS.  This all works as tested by another team member.

--- ProjMgmt/ProjMgmt/urls.py
+++ ProjMgmt/ProjMgmt/urls.py
@@ -4,13 +4,14 @@ from projects import views
 
 urlpatterns = patterns('',
 	
-	url(r'^$', views.HomePage),
+	
 	url(r'^admin', include(admin.site.urls)),
 	
 	url(r'^projects', views.listProjects),
+
 	
 	#Default to login screen
 	#TODO what if the user is already logged in?
-	url(r'^', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    
+	url(r'^login', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^$', views.HomePage),
 )
