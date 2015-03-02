5c0bba16e16ca6394a30f103c92f616a4c87db64
Aidan Thompson <danlidz17@yahoo.com>
2015-03-02 09:35:28 -0500

    Added Edit & Delete Project functionality Created ProjectProperties.html to handle add/edit/delete project actions From projects page, all add/edit/delete actions happen in same window Changed Project form from standard form to ModelForm, which handles field mapping for us
    I temporarily removed the styled Create New Project button that Jared
    created.  We can go back to it, but for testing, I just used the generic
    form.

--- ProjMgmt/ProjMgmt/urls.py
+++ ProjMgmt/ProjMgmt/urls.py
@@ -10,16 +10,16 @@ urlpatterns = patterns('',
 # >>>>>>> pr/9
 	
 	url(r'^admin', include(admin.site.urls)),
-	
 	url(r'^projects/(?P<proj>\d+)', views.project),
-	
 	url(r'^projects', views.listProjects),
-	
-	
 	url(r'^createuser', views.createUser),
+	
+	#Project Add/Edit/Delete
 	url(r'^newproject', views.newproject),
-	url(r'^createProject', views.createProject),
-	#Default to login screen
+	url(r'^editproject/(?P<id>\d+)', views.editproject),
+	url(r'^deleteproject/(?P<id>\d+)', views.deleteproject),
+	
+		#Default to login screen
 	#TODO what if the user is already logged in?
 	url(r'^login', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^$', views.HomePage),
