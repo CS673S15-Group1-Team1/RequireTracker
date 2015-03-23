3bd49303d8958ad3ff184dd0f7544ae299f7de2b
Max Almanzor <max.almanzor@gmail.com>
2015-03-23 19:13:32 -0400

    Add story to iteration

--- ProjMgmt/requirements/models/project.py
+++ ProjMgmt/requirements/models/project.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from base import ProjMgmtBase
-from iteration import Iteration
 
 class Project(ProjMgmtBase):
     #todo add stories
 
     users = models.ManyToManyField(User, through='UserAssociation')
-    iterations = models.ManyToManyField(Iteration)
     
     def __str__(self):
         return self.title
