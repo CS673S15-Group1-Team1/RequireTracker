ae877269ba026c7c2a39b6d84d469b871cfc8303
jaredmael <jaredmael@gmail.com>
2015-03-24 13:54:53 -0400

    Status dropdown (not working)
    Added status dropdown. dropdown works and selectable but wont save to
    django

--- ProjMgmt/requirements/models/story.py
+++ ProjMgmt/requirements/models/story.py
@@ -8,6 +8,13 @@ class Story(ProjMgmtBase):
     project = models.ForeignKey('Project')    
     iteration = models.ForeignKey('Iteration',blank=True,null=True)
     test= models.CharField(default='', max_length=1024, blank=True)
+    status_choices= ( 
+       (1, "Unstarted"),
+       (2, "Started"),
+       (3, "Completed"),
+       (4, "Accepted")
+    )
+    status = models.IntegerField(choices=status_choices, max_length=1, default=1)
     
     def __str__(self):
         return self.title
@@ -25,7 +32,8 @@ def create_story(user, proj, fields):
     story = Story(project=proj,
                   title=fields['title'], 
                   description=fields['description'],
-                  test=fields['test'])
+                  test=fields['test'],
+                  status=['status']),
     story.save()
     return story
 
