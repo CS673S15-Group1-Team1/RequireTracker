e26d2aaa195764810aa29dd25932e0ef3299688d
Stanley Chiang <mraaa@bu.edu>
2015-03-25 20:48:49 -0400

    bugfix/imporve enumiration
    improve enumeration code style in story.py

--- ProjMgmt/requirements/models/story.py
+++ ProjMgmt/requirements/models/story.py
@@ -5,17 +5,29 @@ from project import Project
 from iteration import Iteration
 
 class Story(ProjMgmtBase):
+    STATUS_UNSTARTED = 1
+    STATUS_STARTED = 2
+    STATUS_COMPLETED = 3
+    STATUS_ACCEPTED = 4
+
+    STATUS_CHOICES = (
+        (STATUS_UNSTARTED, "Unstarted"),
+        (STATUS_STARTED, "Started"),
+        (STATUS_COMPLETED, "Completed"),
+        (STATUS_ACCEPTED, "Accepted")
+    )
+
     project = models.ForeignKey('Project')    
     iteration = models.ForeignKey('Iteration',blank=True,null=True)
     reason = models.CharField(default='', max_length=1024,blank=True)
     test= models.CharField(default='', max_length=1024, blank=True)
-    status_choices= ( 
-       (1, "Unstarted"),
-       (2, "Started"),
-       (3, "Completed"),
-       (4, "Accepted")
-    )
-    status = models.IntegerField(choices=status_choices, max_length=1, default=1)
+    # status_choices= ( 
+    #    (1, "Unstarted"),
+    #    (2, "Started"),
+    #    (3, "Completed"),
+    #    (4, "Accepted")
+    # )
+    status = models.IntegerField(choices=STATUS_CHOICES, max_length=1, default=1)
     
     def __str__(self):
         return self.title
