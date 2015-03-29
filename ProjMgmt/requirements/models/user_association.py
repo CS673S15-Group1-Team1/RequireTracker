2c627656d6b0ede05c58f6b5979ee2167da76e44
Amod Lele <amod.lele@gmail.com>
2015-03-29 12:04:45 -0400

    permissions working

--- ProjMgmt/requirements/models/user_association.py
+++ ProjMgmt/requirements/models/user_association.py
@@ -12,51 +12,31 @@ class UserAssociation(models.Model):
     project = models.ForeignKey(Project)
     role = models.CharField(max_length=128)
     
-    def check_permission(permission):
+    def get_permission(self,permission):
         # Checks whether the association's user has the specified permission on the
         # association's project.
-        return permission in get_role_permissions(self.role)
+        return permission in self.get_role_permissions(self.role)
 
 	def test_function():
 		return false
 
-    def get_role_permissions(role):
+    def get_role_permissions(self, role):
         # The role passed should be one of the string constants defined above.
         # This method is where the permissions associated with a role are defined.
         # It should return an array of strings representing permissions.
-        return
-        {
+        role_dictionary = {
             ROLE_CLIENT: ["CreateStory"],
             ROLE_DEVELOPER: ["CreateStory", "EditStory", "DeleteStory"],
             ROLE_OWNER: ["CreateStory", "EditStory", "DeleteStory",
-                          "AddUser", "DeleteUser", "ChangePermissions", "DeleteProject", "AddIteration"],
-        }.get(role, "Error") # "Error" is default if role not found
-        # TODO: proper exception handling for the error.
+                          "AddUser", "DeleteUser", "ChangePermissions", "EditProject", "DeleteProject", "AddIteration"]
+        }
+        # TODO: exception handling if permission not found.
+        return role_dictionary[role]
+
+		
 
     class Meta:  
         app_label = 'requirements'  
      
-
-def get_project_permissions(association):
-    # Returns the permissions attached to the user's role in the project.
-    # Kept separate from get_role_permissions in case we want to have other methods
-    # that use the role/permission mapping in get_role_permissions.
-    return get_role_permissions(association.role)
-
-
-def get_role_permissions(role):
-    # The role passed should be one of the string constants defined above.
-    # This method is where the permissions associated with a role are defined.
-    # It should return an array of strings representing permissions.
-	
-    return
-    {
-        ROLE_CLIENT: ["CreateStory"],
-        ROLE_DEVELOPER: ["CreateStory", "EditStory", "DeleteStory"],
-        ROLE_OWNER: ["CreateStory", "EditStory", "DeleteStory",
-                      "AddUser", "DeleteUser", "ChangePermissions", "DeleteProject", "AddIteration"],
-    }.get(role, "Error") # "Error" is default if role not found
-    # TODO: proper exception handling for the error.
-
 
 
