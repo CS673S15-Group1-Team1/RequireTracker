43e0f1dea842139b896cb78598a9dc34a4b0cfcc
jaredmael <jaredmael@gmail.com>
2015-04-07 21:32:23 -0400

    Model for story comments
    Here is the model for story comments that successfully creates upon make
    migrations so that others can see what I have started

---
+++ ProjMgmt/requirements/models/story_comment.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from base import ProjMgmtBase
+from story import Story
+
+class Story_comment(ProjMgmtBase):
+
+    story = models.ForeignKey(Story)    
+    comment= models.CharField(default='', max_length=1024, blank=True)
+    
+    def __str__(self):
+        return self.title
+        
+    class Meta:
+        app_label = 'requirements'
+
+def get_story_comments(stryID):
+    return Story_comment.objects.filter(story_id=stryID)
+
+def get_story_comment(commentID):
+    return Story_comment.objects.get(id=commentID)
+
+def create_comment(user, stry, fields):
+    if stry is None: return None
+    if Story.objects.filter(id=stry.id).count() == 0: return None
+    if fields is None: return None
+    
+    title = fields.get('title', '')
+    comment = fields.get('comment', '')
+
+    story_comment = Story_comment(story=stry,
+                  comment=comment
+                  )
+    story_comment.save()
+    return story_comment
